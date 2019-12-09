6993b80af660f2906f3540b1b927f4acef87cce4
Chengang <chengang@mokotechnology.com>
2019-12-09 16:41:18 +0800

    Initial commit

---
+++ Example/MKScannerSDK/MKViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKViewController.h
+//  MKScannerSDK
+//
+//  Created by aadyx2007@163.com on 12/09/2019.
+//  Copyright (c) 2019 aadyx2007@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MKViewController : UIViewController
+
+@end
---
+++ Example/MKScannerSDK/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MKScannerSDK
+//
+//  Created by aadyx2007@163.com on 12/09/2019.
+//  Copyright (c) 2019 aadyx2007@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "MKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MKAppDelegate class]));
+    }
+}
